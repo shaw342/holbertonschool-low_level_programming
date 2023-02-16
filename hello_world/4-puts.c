bcecc59f2b0517104d20880471fe22df73e3e0ba
shaw342 <shawanbarua70@gmail.com>
2023-02-16 10:43:12 -0800

    4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+#include <strings.h>
+int main(){
+	
+   char str1[] = "\"Programming is like building a multilingual puzzle";
+
+   puts(str1);
+   
+   
+   return(0);
+}
+	
