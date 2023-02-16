446b24fe5a98e1d548ffa1192a64fee3b3c80c81
shaw342 <shawanbarua70@gmail.com>
2023-02-16 11:42:44 -0800

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <strings.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char str1[] = "\"Programming is like building a multilingual puzzle";
-puts(str1);
+puts(str1)
 return (0);
 }
