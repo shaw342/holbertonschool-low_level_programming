f4d23c38a821e4c3f5613f08d2009525f03711a7
shaw342 <shawanbarua70@gmail.com>
2023-02-16 10:54:32 -0800

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,8 +2,7 @@
 #include <strings.h>
 int main(void)
 {	
- char str1[] = "\"Programming is like building a multilingual puzzle";
- puts(str1);   
- return(0);
-}
-	
+char str1[] = "\"Programming is like building a multilingual puzzle";
+puts(str1);   
+return(0);
+}	
