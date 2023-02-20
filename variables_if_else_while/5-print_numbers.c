314188e9b0e15a2d530b1829da3e44a8c89628b4
shaw342 <shawanbarua70@gmail.com>
2023-02-20 06:59:49 -0800

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+for (int c = 0 ; c < 10 ; c++)
+{
+putchar(c)
+}
+putchar('\n')
+return (0);
+}
