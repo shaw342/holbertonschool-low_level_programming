19644cd69d044a3c7c07b579bc6bf6d4903e2591
shaw342 <shawanbarua70@gmail.com>
2023-02-20 12:43:10 -0800

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+int main(void)
+{
+	print_alphabet(void);
+	return (0);
+}
+void print_alphabet(void)
+{
+	char i;
+
+	for(i = 'a' ; i <= 'z' ; i++){
+	putchar(i);
+	}
+	putchar('\n');
+}
