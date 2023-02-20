1e4c842888b14679c52483f4801d9cf7139505fd
shaw342 <shawanbarua70@gmail.com>
2023-02-20 12:47:55 -0800

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,17 +4,18 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet(void);
 int main(void)
 {
-	print_alphabet(void);
+	print_alphabet();
 	return (0);
 }
 void print_alphabet(void)
 {
 	char i;
 
-	for(i = 'a' ; i <= 'z' ; i++){
+	for(i = 'a' ; i <= 'z' ; i++)
+	{
 	putchar(i);
 	}
 	putchar('\n');
