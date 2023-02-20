2b01150c0aa2e07bed02d35ba038aee1e9928200
shaw342 <shawanbarua70@gmail.com>
2023-02-20 07:48:31 -0800

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(coid)
+{
+int a;
+
+for (a = '0' ; a <= '9' ;a++)
+{
+putchar(a);
+putchar(" ");
+if (a < '9')
+{
+putchar(","
+}
+}
+
+return (0);
+}
