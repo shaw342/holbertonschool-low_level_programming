af75e2fc358db441d4b7c7b26aafbd7c6c1b4b9a
shaw342 <shawanbarua70@gmail.com>
2023-02-21 13:36:05 -0800

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -8,7 +9,7 @@
  * Return: Always 0 (Success)
  */
 
-int print_last_digit(int n);
+int print_last_digit(int n)
 {
 int last;
 
