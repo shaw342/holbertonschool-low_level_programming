32c86c3939dc52db3496507ebed419d827907cdd
shaw342 <shawanbarua70@gmail.com>
2023-02-21 06:08:02 -0800

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_sign - Check Holberton
+ * @n: An input character
+ * Return: 1 if > 0, 0 if =0, -1 if < 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+printf("+");
+return (1);
+}else if ( n == 0)
+{
+printf("0");
+return (0);
+}else
+{
+printf("-1");
+}
+}
