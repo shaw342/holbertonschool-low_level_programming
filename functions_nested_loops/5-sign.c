f50e035a780e7fa9e63049b5a36d79f05689670e
shaw342 <shawanbarua70@gmail.com>
2023-02-21 06:26:02 -0800

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -7,18 +7,20 @@
  */
 int print_sign(int n)
 {
+int value;
 if (n > 0)
 {
 putchar('+');
-return (1);
+value = 1;
 }
 else if ( n == 0)
 {
 putchar('0');
-return (0);
+value = 0;
 }else
 {
 putchar('-');
-return (-1);
+value = -1;
 }
+return value;
 }
