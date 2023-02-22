3da092b1fe12dc0cf37c2964ee853e0a5d2ee4b4
shaw342 <shawanbarua70@gmail.com>
2023-02-22 08:57:32 -0800

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,24 +1,39 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ *times_table - print the multiplication table 0 to 10
+ *
+ */
 void times_table(void)
 {
 	int i;
 	int b;
 	int c;
-	
+
 	for (i = 0 ; i <= 9 ; i++)
 	{
 	for (b = 0 ; b <= 9 ; b++)
 	{
-	c = i * b ;
+	c = i * b;
+	if (c > 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(c / 10 + '0');
+	_putchar(c % 10 + '0');
+	}
+	else if (c == 0)
+	{
 	_putchar(c + '0');
-	
 	}
-	
-	_putchar('\n');
+	else
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(c + '0');
 	}
-
-
-
-
+	}
+	putchar('\n');
+	}
 }
