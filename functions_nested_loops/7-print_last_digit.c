8aee2ea828aa15171b8319bb7f7a91753571c946
shaw342 <shawanbarua70@gmail.com>
2023-02-22 00:48:29 -0800

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,31 +2,15 @@
 #include <time.h>
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int print_last_digit(int n)
 {
 int last;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-last = n % 10;
-if (last > 5)
-{
-printf("%d%d\n", last,last);
-}
-else if (last == 0)
-{
-printf("%d%d\n", last,last);
-}
-else
-{
-printf("%d%d\n", last,last);
-}
+last  = n%10;
+printf("%d",last);
 return (0);
 }
