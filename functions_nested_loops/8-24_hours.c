61ecb694f5f53161dc2143116a78f4fcd36dc71f
shaw342 <shawanbarua70@gmail.com>
2023-02-22 06:43:16 -0800

    8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,13 +1,35 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * jack_bauer - print hours
+ * Return: always last digit
+ */
 void jack_bauer(void)
 {
-char i;
+int a,b,c,d;
 
 
-	for (i = 0; i < 60 ;i++)
+	for (a = 0; a <= 2 ; a++)
 	{ 
-	_putchar(i);
+	for (b = 0; b <= 9; b++)
+	{
+	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+	{
+	for (c = 0 ; c <= 5 ; c++)
+	{
+	for (d = 0 ; d <= 9 ; d++)
+	{ 
+	_putchar(a + '0');
+	_putchar(b + '0');
+	_putchar(':');
+	_putchar(c + '0');
+	_putchar(d + '0');
+	_putchar('\n');
+	}
+	}
+	}
+	
+	} 
 	}
 }
