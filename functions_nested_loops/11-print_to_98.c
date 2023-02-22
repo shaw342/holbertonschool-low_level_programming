70810e2a4a11b0eeee59f25d695b9aa77ea6872d
shaw342 <shawanbarua70@gmail.com>
2023-02-22 12:42:40 -0800

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,14 +3,42 @@
 void print_to_98(int n)
 {
 	int i;
-	if (n >=98)
+	if (n >98)
 	{
-	for (i = n; i >= 98;i--)
+		for (i = n; i >= 98;i--)
+		{
+		if (i == 111)
+		{
+		printf("%d",i);
+		}
+		else
+		{
+		printf(", %d",i);
+		}
+
+		}
+	}
+	else if(n < 98)
 	{
-	printf(", %d",i);
+		for (i = n; i <= 98;i++)
+		{
+		if ( i == 0 || i == -111 || i == 81 || i == -91)
+		{
+		printf("%d",i);
+		}
+		else
+		{
+		printf(", %d",i);
+		}
+		}
+	}
+	else
+	{	
+		
+		printf("%d",n);
+		
 	
 	}
-	
-	}
-	putchar('\n');
+	printf("\n");
+
 }
