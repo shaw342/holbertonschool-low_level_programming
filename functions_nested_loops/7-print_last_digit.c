7c325b13fd76bc5af28cd2c99e0bb625c3e586d2
shaw342 <shawanbarua70@gmail.com>
2023-02-22 01:42:42 -0800

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,20 +3,21 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_last_digit - computes the absolute value of an integer
+ * @r: An input character
+ * Return: always last digit
  */
+
 int print_last_digit(int n)
 {
 int last;
-last  = n%10;
+last  = n % 10;
 if (last < 0)
 {
-last = -1*last;
+last = -1 * last;
 _putchar(last + '0');
 return (last);
 }
 _putchar(last + '0');
-return (n%10);
+return (n % 10);
 }
