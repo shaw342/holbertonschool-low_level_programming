28174207c46381e19a986e0b31478ac645e5bfbb
shaw342 <shawanbarua70@gmail.com>
2023-02-23 04:44:07 -0800

    1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,16 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+/**
+ * _isdigit - Check Holberton
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
 int _isdigit(int c)
 {	char l = (char)c;
 
-	if(isdigit(l))
+	if( isdigit(l))
 	{
-	return(1);
+	return (1);
 	}
 	else
 	{
-	return(0);
+	return (0);
 	}
 	
 }
