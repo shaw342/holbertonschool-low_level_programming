bccfbc18277aef8995b0cfc55b1d531fa2cff58e
shaw342 <shawanbarua70@gmail.com>
2023-02-23 05:33:13 -0800

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -17,10 +17,10 @@ void more_numbers(void)
 	{
 	for (j = 0; j <= 15 ; j++)
 	{
-	putchar(j / 10);
-	putchar(j % 10 + '0');
+	_putchar(j / 10);
+	_putchar(j % 10 + '0');
 	}
-	putchar('\n');
+	_putchar('\n');
 	
 	}
 }
