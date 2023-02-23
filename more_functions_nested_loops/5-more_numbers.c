c71d2d20d78df7acb38cb06819ad0e3e4da3c59e
shaw342 <shawanbarua70@gmail.com>
2023-02-23 05:29:36 -0800

    4

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_numbers - Check Holberton
+ * Description: function uses _putchar function to print
+ * print - the number from 0 to 9
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10 ; i++)
+	{
+	for (j = 0; j <= 15 ; j++)
+	{
+	putchar(j + '0');
+	}
+	putchar('\n');
+	
+	}
+}
