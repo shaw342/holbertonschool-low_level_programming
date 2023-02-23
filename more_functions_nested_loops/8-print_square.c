e29a85d8f78e77447198aaa622b099fbe86c80a1
shaw342 <shawanbarua70@gmail.com>
2023-02-23 09:02:58 -0800

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_square - Check Holberton
+ *@n: an input number
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+
+	for (i = 0; i <= size ; i++)
+	{
+	for (j = 0; j <= i ; j++)
+	{
+	_putchar('*');
+	}
+	_putchar('\');
+	}
+
+
+}
