0ce2c303f4a20c140dd274d64cb257933c412486
shaw342 <shawanbarua70@gmail.com>
2023-02-23 04:53:49 -0800

    2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include <stdio.h>
-/*
- * File: 1-alphabet.c
- * Auth: Brennan D Baraban
- */
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ * mul - Check Holberton
+ * @a: An input character
+ * @b: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
  */
 int mul(int a, int b)
 {
-int c = a * b;	
+int c = a * b;
 return (c);
 }
