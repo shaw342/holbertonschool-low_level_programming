18709b5f159f520ffe7d4fb68e8690648a387e28
shaw342 <shawanbarua70@gmail.com>
2023-02-23 04:10:14 -0800

    0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - Check Holberton
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in uppercase 10 times
+ * Return: 1 if is uppercase or 0 if is uppercase
+ */
+int _isupper(int c)
+{
+        char l = (char) c;
+
+        if (isupper(l))
+        {
+        return (1);
+        }
+        else
+        {
+        return (0);
+        }
+
+
+
+
+}a
