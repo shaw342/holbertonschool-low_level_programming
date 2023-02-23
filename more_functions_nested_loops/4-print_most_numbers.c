6e245507bf4373df916f38849bac4c5f6086b73d
shaw342 <shawanbarua70@gmail.com>
2023-02-23 05:13:32 -0800

    4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_numbers - Check Holberton
+ * Description: function uses _putchar function to print
+ * print - the number from 0 to 9
+ */
+
+void  print_numbers(void)
+{
+        int i;
+	
+	
+        for (i = 0; i < 10; i++)
+        {
+	if(i != 2 && i != 4)
+	{
+        _putchar(i  + '0');
+	}
+        }
+        _putchar('\n');
+}
