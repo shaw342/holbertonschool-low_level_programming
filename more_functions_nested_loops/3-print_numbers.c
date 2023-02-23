a5e710166e726dc2e7b108c77b1961b92c7a7026
shaw342 <shawanbarua70@gmail.com>
2023-02-23 05:00:23 -0800

    2

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * mul - Check Holberton
+ * @a: An input character
+ * @b: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+
+void mul(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	_putchar(i +'0');
+	}
+	putchar('\n');
+}
