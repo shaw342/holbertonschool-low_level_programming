a8bfbf69af4bcb3d5f599498dd80c563037b5305
shaw342 <shawanbarua70@gmail.com>
2023-02-23 08:53:02 -0800

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -11,22 +11,19 @@
  */
 void print_diagonal(int n)
 {
-        int i;
+	int i;
 	int j;
 
-        for (i = 0; i < n ; i++)
-        {
-        for (j = 0; j < i ; j++)
+	for (i = 0; i < n ; i++)
+	{
+	for (j = 0; j < i ; j++)
 	{
 	_putchar(' ');
-        }
+	}
 	_putchar('\\');
 	_putchar('\n');
-	
-
 	}
-	
-	if(n == 0)
+	if (n <= 0)
 	{
 	_putchar('\n');
 	}
