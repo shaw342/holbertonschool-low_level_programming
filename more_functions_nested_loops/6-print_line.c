c2d7fab55169a285fe2081c35d3d06859fccaeb9
shaw342 <shawanbarua70@gmail.com>
2023-02-23 05:55:02 -0800

    -

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * more_numbers - Check Holberton
+ * Description: function uses _putchar function to print
+ * print - the number from 0 to 14
+ */
+
+void print_line(int n)
+{
+	 int i;
+
+	 for (i = 0; i <= n ; i++)
+	 {
+	 if ( n <= 0 &&  )
+	 {
+	 putchar('\n');
+	 
+	 }
+	 putchar('-');
+	 }
+}
