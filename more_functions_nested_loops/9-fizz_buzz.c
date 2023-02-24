d8373dd5b72e21f67d4ec93fe46598bf9029b702
shaw342 <shawanbarua70@gmail.com>
2023-02-24 04:46:55 -0800

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,22 +10,14 @@ int main(void)
 	for (i = 1; i <= 100 ; i++)
 	{
 	
-	if (i % 3 == 0)
-	{
-		printf(" Fizz");
-	}
-	else if (i % 5 == 0)
-	{
-		printf(" Buzz");
-	}
-	else if (i % 5 == 0 && i % 3 == 0)
-	{
-	printf("FizzBuzz");
-	}
-	else
-	{
-	printf(" %d", i);
-	}
+		if (i % 5 == 0 && i % 3 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
 	}
 	printf("\n");
 
