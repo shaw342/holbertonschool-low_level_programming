ba7bec48ead3fdbf6c12b7b95515de447d9c6465
shaw342 <shawanbarua70@gmail.com>
2023-02-24 06:19:44 -0800

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+void print_triangle(int size)
+{
+	int i;
+	int c;
+	if( size != 0)
+	{
+		for (i = size ; i >= 0 ; i--)
+		{
+			for (c = 1 ; c <= size ; c++)
+			{
+				if(c >= i)
+					_putchar('*');
+				else	
+					_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	
+	}
+
+}
