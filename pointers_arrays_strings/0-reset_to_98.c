31c94aa3ed912dde938c4c96bdcc9077a50889c0
shaw342 <shawanbarua70@gmail.com>
2023-02-27 01:19:36 -0800

    fixed bug

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * reset_to_98 - function that takes a pointer to an int as parameter and updates the value it points to to 98
+ /**
+ * reset_to_98 - function that takes a int and updates the value
  * @*n:pointing in &n
  * */
 void reset_to_98(int *n)
--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -13,5 +13,4 @@ void swap_int(int *a, int *b)
 	temp = *a;
 	*a = *b;
 	*b = temp;
-
 }
