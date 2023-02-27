6edc7ca811fed478545e24412fc8936124396fb3
shaw342 <shawanbarua70@gmail.com>
2023-02-27 03:14:30 -0800

    3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line
+ * @str: strings
+ * Return: void
+ */
 void _puts(char *str)
 {
-	printf("%s\n",str);
+	printf("%s\n", str);
 }
