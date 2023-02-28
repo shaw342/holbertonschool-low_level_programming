701e27eb47d9492608de2c2c1095d7ee020364d1
shaw342 <shawanbarua70@gmail.com>
2023-02-28 07:29:32 -0800

    9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * *_strcpy - check holberton
+ * @dest: have strings
+ * @src:have strings
+ * Return:void
+ */
 char *_strcpy(char *dest, char *src)
 {
- 	int i;
+	int i;
 	
-	for (i = 0; src[i] != '\0';++i)
+	for (i = 0; src[i] != '\0'; ++i)
 	{
 	dest[i] = src[i];
 	}
