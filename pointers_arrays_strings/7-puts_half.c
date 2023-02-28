65740d6e31a58a236b59bb4eb2eb6677b9a5646f
shaw342 <shawanbarua70@gmail.com>
2023-02-28 05:39:52 -0800

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * puts_half - check by holberthon
+ * @str: have the strig
+ * return: void
+ */
 void puts_half(char *str)
 {
-	int i=0;
+	int i = 0;
 	int j;
 	
-
 	while (str[i] != '\0')
-	{	
-	
-	i++;	
-	}
-	i = i+1;
-	i = i/2;
-	for(j = i; str[j] != '\0';j++)
 	{
-			putchar(str[j]);
+	i++;
+	}
+	i = i + 1;
+	i = i / 2;
+	for (j = i; str[j] != '\0'; j++)
+	{
+		putchar(str[j]);
 	}
 	putchar('\n');
 }
