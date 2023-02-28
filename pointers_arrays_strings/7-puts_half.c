68643027499d754af50aaa041ec43287316a71ce
shaw342 <shawanbarua70@gmail.com>
2023-02-28 05:28:12 -0800

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include "main.h"
-void puts2(char *str)
+void puts_half(char *str)
 {
+	int i=0;
+	int j;
+	int leng;
+
+	while (str[i] != '\0')
+	{	
 	
+	i++;	
+	}
+	leng = i/2;
+	for(j = leng; str[j] != '\0';j++)
+	{
+			putchar(str[j]);
+	}
+	putchar('\n');
 }
