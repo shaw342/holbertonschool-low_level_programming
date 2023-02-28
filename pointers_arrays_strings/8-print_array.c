f0790e5f2747c1c0672c99a72a856b929d390c98
shaw342 <shawanbarua70@gmail.com>
2023-02-28 06:25:08 -0800

    8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,14 +11,14 @@ void print_array(int *a, int n)
 	if (i != n -1 )
 	{
 
-
-	for (i = 0 ; i < n ;i++)
-	{
-		printf("%d, ", a[i]);
-	}
+	printf("%d, ", a[i]);
+	i++;
 	}
 	else
+	{
 	printf("%d, ",a[i]);
+	i++;
+	}
 	}
 	putchar('\n');
 
