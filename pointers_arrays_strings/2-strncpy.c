98a6368a371a1704fedea891449fd6505c288bef
shaw342 <shawanbarua70@gmail.com>
2023-03-01 05:58:06 -0800

    2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strncat - function
+ * @dest: containe the sting
+ * @src: containe the string
+ * @n: containe integer
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int y;
+
+	while (dest[i] != '\0')
+        {
+		i++;
+	}
+	for (y = dest[i]; y < n && src[y] != '\0'; y++)
+        {
+		dest[i] = src[y];
+	}
+	dest[i] = '\0';
+	
+	return (dest);
+}
