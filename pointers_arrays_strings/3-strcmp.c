90ccd9198db5e2034256fe5d2bdf25e24277e1cc
shaw342 <shawanbarua70@gmail.com>
2023-03-01 07:20:22 -0800

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * _strcmp - function
+ * @s1:have caractere
+ * @s2:have caractere
+ * Return:0 or s1 - s2
+ */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
 	while (s2[i] != '\0' && s1[i] != '\0')
-	{	
+	{
 		if (s1[i] != s2[i])
 		{
-
-		
 		return (s1[i] - s2[i]);
 		}
 		i++;
