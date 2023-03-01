c930b9c5e0c2fc9bb62934245f4297cdaf253e46
shaw342 <shawanbarua70@gmail.com>
2023-03-01 08:22:01 -0800

    5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,9 +2,15 @@
 #include "main.h"
 #include <ctype.h>
 
+/**
+ **string_toupper - function
+ *@n: have string
+ *Return: n
+ */
 char *string_toupper(char *n)
 {
 	int i = 0;
+
 	while (n[i] != '\0')
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
