2422ef0ecc175eb791d2bbbd4ab7d701b0ed1bbd
shaw342 <shawanbarua70@gmail.com>
2023-03-01 05:45:22 -0800

    1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include "main.h"
+
+/**
+ * *_strncat - function
+ * @dest: containe the sting
+ * @src: containe the string
+ * return: dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int y;
-	
+
 	while (dest[i] != '\0')
 	{
 		i++;
-	}	
-	for(y = dest[i];y < n && src[y] != '\0'; y++)
+	}
+	for (y = dest[i]; y < n && src[y] != '\0'; y++)
 	{
 		dest[i++] = src[y];
 	}
