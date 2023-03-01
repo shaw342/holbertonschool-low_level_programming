1f3cf4099404026cf85ff04763619b7694705417
shaw342 <shawanbarua70@gmail.com>
2023-03-01 08:40:35 -0800

    6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+char *cap_string(char *a)
+{
+int i;
+
+for (i = 0; a[i]; i++)
+{
+if (a[i] == "." && a[i] == ',' && a[i] == "\"" && a[i] == " " && a[i] == "{" && a[i] == "}" && a[i] == "(" && a[i] == ")" && a[i] == ";" && a[i] == "!")
+{
+	a[i+1] = (int)a[i+1] - 32;
+}
+
+}
+return (a);
+}
