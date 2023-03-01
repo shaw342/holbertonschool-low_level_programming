9decf363095ea567367ca24333b1430c7943e04d
shaw342 <shawanbarua70@gmail.com>
2023-03-01 06:04:30 -0800

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,9 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 	int y;
 
-	while (dest[i] != '\0')
-        {
-		i++;
-	}
 	for (y = dest[i]; y < n && src[y] != '\0'; y++)
         {
-		dest[i] = src[y];
+		dest[i] = src[i];
 	}
 	dest[i] = '\0';
 	
