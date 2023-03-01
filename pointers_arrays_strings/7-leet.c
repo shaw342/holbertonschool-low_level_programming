bf7da7c1f7d6e354a86910ccda0f72b161521c1c
shaw342 <shawanbarua70@gmail.com>
2023-03-01 14:12:20 -0800

    7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+char *leet(char *str)
+{
+	int i;
+	int y;
+	char r[] = "aAeEoOtTlL";
+	char m[] = "4433007711";
+
+
+	while (str[x])
+	{
+		for (s = 0 ; r[s] ;s++)
+		{
+			if (str[x] == r[s])
+			{
+				r[s] = m[s]
+			}
+		}	
+	}
+	
+return (str);
+}
