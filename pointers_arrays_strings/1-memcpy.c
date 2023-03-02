a2923a9707a594745ef4d2196f0c3276b52810dc
shaw342 <shawanbarua70@gmail.com>
2023-03-02 02:58:33 -0800

    1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include <stido.h>
+#include "main.h"
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i != n)
+	{
+		dest[i++] = '\0';
+	}
+
+	return (dest);
+
+
+	
+}
