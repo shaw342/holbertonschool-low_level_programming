0daf60dab722cf345df14bd94ca1db1b6241b72e
shaw342 <shawanbarua70@gmail.com>
2023-03-02 11:00:09 -0800

    3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int l;
+
+	while (s[i] != '\0')
+	{
+		for (l = 0; accept[l] ; l++)
+		{
+			if(s[i] == accept[l])
+			{
+				l++
+			}
+		}
+
+	i++;
+	}
+return (s);
+}
