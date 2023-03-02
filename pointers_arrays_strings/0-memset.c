055b839747067557fd47676e71ede7a2cfe63b55
shaw342 <shawanbarua70@gmail.com>
2023-03-02 02:23:59 -0800

    0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[x] = b;
+	}
+return (0);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,3 +14,4 @@ char *_strcat(char *dest, char *src);
 int main(void);
 char *_strncpy(char *dest, char *src, int n);
 char *cap_string(char *a);
+char *_memset(char *s, char b, unsigned int n);
