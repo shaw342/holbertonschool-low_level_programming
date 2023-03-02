146e2305f14fd017163dc400d131f539b012e1fe
shaw342 <shawanbarua70@gmail.com>
2023-03-02 06:09:48 -0800

    2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include "main.h"
+
+/**
+ * *_strchr - function
+ * @s: strings
+ * @c:value
+ * Return: s or NULL
+ */
 char *_strchr(char *s, char c)
 {
 
 	while (*s != '\0')
 	{
 
-	if(*s == c)
+	if (*s == c)
 	{
 		return (s);
 
 	}
 	s++;
 	}
-	if(c == '\0')
+	if (c == '\0')
 	{
 		return (s);
 	}
