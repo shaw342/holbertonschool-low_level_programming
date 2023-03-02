822c4d3c12b5585a258bc1daa217c6d69a9cd366
shaw342 <shawanbarua70@gmail.com>
2023-03-02 02:28:30 -0800

    0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include "main.h"
+
+/**
+ * *memeset - fucntion
+ * @s: pointers
+ * @b: variable 
+ * @n: octes
+ * Return:s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
