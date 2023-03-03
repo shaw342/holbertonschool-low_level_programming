7d44388cd91d2d1674ea325262ff27483a11a829
shaw342 <shawanbarua70@gmail.com>
2023-03-03 04:42:25 -0800

    3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,6 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i = 0;
 	unsigned int l = 0;
 	
-
 	while (s[i] != '\0')
 	{
 		
@@ -14,23 +13,18 @@ unsigned int _strspn(char *s, char *accept)
 
 			if (s[i] == accept[l])
 			{
-					return (s);
 					break;
 			}
 			if (accept[l] == '\0')
 			{
-				return (s);
+				
 				break;
 			}
 			l++;
 		}
 
-			
-
 		i++;	
 	}
 
-	
-	
-return (NULL);
+return (i);
 }
