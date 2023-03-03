24f08710e7f71665d95bb6dfb4bf493ef76ed4be
shaw342 <shawanbarua70@gmail.com>
2023-03-03 06:43:59 -0800

    4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int r;
+
+	while (s[i])
+	{
+		while (accept[r])
+		{
+			if (s[i] == accept[r]
+			{
+				break;
+			}
+			l++;
+
+		}
+			if(accept == '\0')
+			{
+				break;
+			}
+	i++;
+	}
+
+return (s);
+}
