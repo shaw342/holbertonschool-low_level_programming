65611a5d371202a8ff6b5a54491e3b74d2914e06
shaw342 <shawanbarua70@gmail.com>
2023-03-04 04:23:48 -0800

    5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+char *_strstr(char *haystack,char *needle)
+{
+	while (*s != '\0')
+	{
+        if(*accept == *s)
+        {
+          return (s);
+        }
+     s++;
+     }
+
+
+return (NULL);
+}
