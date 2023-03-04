93b6da020fe2fcab7d29c3e8b256cb14fdd7f998
shaw342 <shawanbarua70@gmail.com>
2023-03-04 11:23:41 -0800

    5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -2,15 +2,21 @@
 #include "main.h"
 char *_strstr(char *haystack,char *needle)
 {
-	while (*haystack != '\0')
-	{
-        if(*needle == *haystack)
-        {
-          return (haystack);
-        }
-     haystack++;
-     }
+  int i;
+  int l;
 
+  
+  for (i = 0;haystack[i];i++)
+    {
+      for (l = 0;needle[l];l++)
+        {
+          if(haystack[i] == needle[l] && haystack[i+1] == needle[l+1]){
+
+            return (haystack + i);
+          }
+        }
+      
+    }
 
 return (NULL);
 }
