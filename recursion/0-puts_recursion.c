b3e5b2fed06f1602ce16c38da4be394a51092bf3
shaw342 <shawanbarua70@gmail.com>
2023-03-06 05:22:39 -0800

    0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _puts_recursion - puts string
+ * @s:output
+ * Return:void
+ */
 void _puts_recursion(char *s)
 {
-	printf("%s\n",s);
+	printf("%s\n", s);
 }
