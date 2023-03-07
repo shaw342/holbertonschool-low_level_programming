090a1b5d113f3ce6ef25c7054783d6b5a623e0d9
shaw342 <shawanbarua70@gmail.com>
2023-03-07 03:11:09 -0800

    1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * _print_rev_recursion - function
+ * @s: input
+ * Return: void
+ */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		putchar(*s++);
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+
 	}
-	s--;
-	_print_rev_recursion(s);
+
 }
