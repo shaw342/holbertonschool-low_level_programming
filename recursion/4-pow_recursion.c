a27dfad2033e8d272dd0653596a63627b6bed6ad
shaw342 <shawanbarua70@gmail.com>
2023-03-08 02:45:34 -0800

    4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <math.h>
+/**
+ * _pow_recursion - function
+ * @x:input
+ * @y:input
+ * Return:x power y
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
@@ -8,7 +14,7 @@ int _pow_recursion(int x, int y)
 	}
 	if (y > 0)
 	{
-	return (x * _pow_recursion(x,y - 1));
+	return (x * _pow_recursion(x, y - 1));
 	}
 	else
 	{
