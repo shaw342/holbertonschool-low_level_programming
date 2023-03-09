1e5a40eb25f0ae84370587fe0a83522ecce9c386
shaw342 <shawanbarua70@gmail.com>
2023-03-09 05:15:21 -0800

    6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,24 +1,35 @@
 #include "main.h"
 #include <stdio.h>
-int check(int n,int i);
+/**
+ * is_prime_number - function
+ * @n: input
+ * @i:input
+ * Return: function check
+ */
+int check(int n, int i);
 int is_prime_number(int n)
 {
-	if (n <= 0)
+	if (n <= 1)
 	{
 		return (0);
 	}
-	return (check(n,2));
+	return (check(n, 2));
 }
-int check(int n,int i)
+/**
+ * check - function
+ * @n: input
+ * @i: input
+ * Return: check
+ */
+int check(int n, int i)
 {
 	if (n == i)
 	{
-		
 		return (1);
 	}
 	if (n % i == 0)
 	{
 		return (0);
 	}
-	return (check(n,i + 1));
+	return (check(n, i + 1));
 }
