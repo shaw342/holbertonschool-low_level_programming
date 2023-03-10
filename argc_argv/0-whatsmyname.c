d01e271e0cfb4049ecc47f15cc5d0d282173861e
shaw342 <shawanbarua70@gmail.com>
2023-03-10 08:25:24 -0800

    0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,10 +3,11 @@
 #include "main.h"
 /**
  * main - check the code
- *
+ * @argv: argument value, a strings that come after calling function
+ * @argc: argument count
  * Return: Always 0.
  */
-int main ( __attribute__((unused))int argc, __attribute__((unused)) char **argv)
+int main(__attribute__((unused))int argc, __attribute__((unused))char **argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);
