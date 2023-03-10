e997c3a5ea048df1d396f563f529a14f2311d6f4
shaw342 <shawanbarua70@gmail.com>
2023-03-10 11:24:41 -0800

    4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,30 +1,59 @@
 #include <stdio.h>
-#include "main.h"
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 /**
- * main - A program that adds two numbers
- * @argc: The arguments' counter
- * @argv: The arguments' values
- * Return: Always 0 (Success)
+ * get_number - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-int main(__attribute__((unused))int argc, __attribute__((unused))char **argv)
-{
-	int sum = 0, i;
 
-	if (argc < 1)
-	{
-		return (0);
-	}
+int get_number(int argc, char *argv[])
+
+
+{
+	int i, sum = 0;
+
+
 	for (i = 1; i < argc; i++)
+
 	{
-	if (isdigit(argv[i]))
-	{
-		printf("Error\n");
-		return (1);
+		int j;
+
+		for (j = 0; j < (int)strlen(argv[i]); j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
 	}
-	sum += atoi(argv[i]);
-	}
+
 	printf("%d\n", sum);
+	return (0);
+}
+
+/**
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+
+{
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+	else
+	{
+		return (get_number(argc, argv));
+	}
 	return (0);
 }
