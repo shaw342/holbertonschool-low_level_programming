f6826b58bf541290f9ad76d2d50fed8988bfad47
shaw342 <shawanbarua70@gmail.com>
2023-03-10 04:33:13 -0800

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+int main( __attribute__((unused))int argc, __attribute__((unused)) char** argv)
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
