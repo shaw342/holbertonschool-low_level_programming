b598d3de70f3d64f9ad5f04a1293658ca817f74b
shaw342 <shawanbarua70@gmail.com>
2023-03-10 10:55:21 -0800

    4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+int main(__attribute__((unused))int argc, __attribute__((unused))char **argv)
+{
+	int sum = 0,i;
+
+	if (argc < 1)
+	{
+		return (0);
+	}
+	for (i = 0; i < argc;i++)
+	{
+	 if (!atoi(argv[i]))
+        {
+                printf("Error\n");
+		return (1);
+        }
+		
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n",sum);
+	return (0);
+}
