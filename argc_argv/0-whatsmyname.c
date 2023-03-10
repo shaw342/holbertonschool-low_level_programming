0c3f12f6c01838619f41a9c92eee1d9b7d10fdfb
shaw342 <shawanbarua70@gmail.com>
2023-03-10 08:16:46 -0800

    0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-int main( __attribute__((unused))int argc, __attribute__((unused)) char** argv)
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main ( __attribute__((unused))int argc, __attribute__((unused)) char **argv)
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
