3940dcbb45d88f8c903e060dfaca9d6edbd36119
shaw342 <shawanbarua70@gmail.com>
2023-03-10 08:33:50 -0800

    1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+/**
+ * main - check the code
+ * @argc: count argument
+ * @argv: argument value, a strings that come after calling function
+ * Return: Always 0.
+ */
 int main(__attribute__((unused))int argc, __attribute__((unused))char **argv)
 {
 	printf("%d\n", argc);
