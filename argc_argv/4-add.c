0bfaf0071f5bd515377dd99ac6fe773368c796e3
shaw342 <shawanbarua70@gmail.com>
2023-03-10 11:02:18 -0800

    4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,24 +2,29 @@
 #include "main.h"
 #include <stdlib.h>
 #include <ctype.h>
+/**
+ * main - A program that adds two numbers
+ * @argc: The arguments' counter
+ * @argv: The arguments' values
+ * Return: Always 0 (Success)
+ */
 int main(__attribute__((unused))int argc, __attribute__((unused))char **argv)
 {
-	int sum = 0,i;
+	int sum = 0, i;
 
 	if (argc < 1)
 	{
 		return (0);
 	}
-	for (i = 0; i < argc;i++)
+	for (i = 1; i < argc; i++)
 	{
-	 if (!atoi(argv[i]))
-        {
-                printf("Error\n");
+	if (!atoi(argv[i]))
+	{
+		printf("Error\n");
 		return (1);
-        }
-		
-		sum += atoi(argv[i]);
 	}
-	printf("%d\n",sum);
+	sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
