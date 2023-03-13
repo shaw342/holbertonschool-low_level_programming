09b9da736db16419a6c431bd96e3abc49fa9596a
shaw342 <shawanbarua70@gmail.com>
2023-03-13 08:03:10 -0700

    1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - functiion
+ * @str: input
+ * Return: array
+ */
+char *_strdup(char *str)
+{
+	unsigned int i;
+	char *array;
+	
+	if (str == 0)
+	{
+		return (0);
+	}
+	for ( i = 0; str[i]; i++)
+		;
+
+	array = malloc((i) * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for ( i = 0;str[i];i++)
+	array[i] = str[i];
+	array[i] = '\0';	
+	return (array);
+}
