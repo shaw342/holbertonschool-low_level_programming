5029651765afb802120bd5d58cb1fcdb3b406fab
shaw342 <shawanbarua70@gmail.com>
2023-03-13 05:05:59 -0700

    0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * *create-array - creates an array of chars, and initializes it with a specific char
+ * create_array - creates an array
  * @size: input
  * @c: input
  * Return: array
