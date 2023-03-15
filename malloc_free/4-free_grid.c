7b1f5b543359cd64971a42761eb5bf63cd27d756
shaw342 <shawanbarua70@gmail.com>
2023-03-15 08:59:09 -0700

    4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * free_grid - function
+ * @grid: input
+ * @height: input
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i
+
+	for (i = 0; i < height; i++)
+		free(grid[i];
+	free(grid);
+}
