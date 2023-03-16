e60a8fcdadd484572b798b4102c11d913a28feb3
shaw342 <shawanbarua70@gmail.com>
2023-03-16 07:36:39 -0700

    1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s2[i]; i++)
 		l++;
 
-	array = malloc(l * sizeof(char) + 1);
+	array = malloc((l * sizeof(char)) + 1);
 
 	if (array == NULL)
 	{
