dad102cd28dccccf3c17f19b28000df04fbea3d7
shaw342 <shawanbarua70@gmail.com>
2023-03-20 03:06:37 -0700

    0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,15 +1,19 @@
-#ifdef HEADER
-#define HEADER
+#ifdef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct
+ * @name: char ptr
+ * @age: float
+ * @owner: char ptr
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-
+} dog_t;
+void  init_dog(struct dog *d, char *name, float age, char *owner);
+void  print_dog(struct dog *d);
+dog_t  *new_dog(char *name, float age, char *owner);
+void  free_dog(dog_t *d);
 #endif
