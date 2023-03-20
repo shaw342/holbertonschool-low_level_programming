18a8ef5d6ddc20eb2cf0545ae2e3610e9f6f8c96
shaw342 <shawanbarua70@gmail.com>
2023-03-20 08:51:01 -0700

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
 #include "dog.h"
-int _sterlen(char *s);
-char _strcpy(char *dest, char *src);
+#include <stdlib.h>
+char *_strdup(char *str);
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int long1;
-	int long2;
 	char *cpyowner;
 	char *cpyname;
 	dog_t *new_dog;
 
-	long1 = _strlen(name);
-	long2 = _strlen(owner);
+	if (owner == NULL && name == NULL)
+	{
+		return (NULL);
+	}
 
-
-	dog = malloc(sizeof(dog_t));
-	cpyname = malloc((sizeof(char) * long1) + 1);
+	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-
+	cpyname = _strdup(name);
 	if (cpyname == NULL)
 	{
 		return (NULL);
 	}
-	cpyname = _strcpy(name);
-	cpyowner = mallc((sizeof(char) * long2) + 1);
+	cpyowner = _strdup(owner);
 
 	if (cpyowner == NULL)
 	{
@@ -36,7 +33,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(cpyname);
 		return (NULL);
 	}
-	cpyowner = _strcpy(owner);
 
 	new_dog->name = cpyname;
 	new_dog->owner = cpyowner;
@@ -45,25 +41,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 
 }
-int _sterlen(char *s)
+/**
+ * _strdup - functiion
+ * @str: input
+ * Return: array
+ */
+char *_strdup(char *str)
 {
 	int i;
+	char *array;
 
-	while (s[i])
+	if (str == 0)
 	{
-		i++;
+		return (0);
 	}
-	return (i);
-}
-char _strcpy(char *dest, char *src)
-{
-	int i;
+	for (i = 0; str[i]; i++)
+		;
 
-	while (src[i])
+	array = malloc((i + 1) * sizeof(char));
+	if (array == NULL)
 	{
-		dest[i] = src[i];
-		i++;
+		return (NULL);
 	}
-	return (dest);
-
+	for (i = 0; str[i] ; i++)
+	array[i] = str[i];
+	array[i] = '\0';
+	return (array);
 }
