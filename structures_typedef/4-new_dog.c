c7269583c7237b3ac76ce84c7453c78c35a14251
shaw342 <shawanbarua70@gmail.com>
2023-03-20 08:54:05 -0700

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
+/**
+ * new_dog - function
+ * @name: input
+ * @age: input
+ * @owner: input
+ * Return: new_dog
+ */
 char *_strdup(char *str);
 dog_t *new_dog(char *name, float age, char *owner)
 {
