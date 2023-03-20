b54b3adcf869366360bf13890902b9b2ec93e8e5
shaw342 <shawanbarua70@gmail.com>
2023-03-20 03:42:05 -0700

    1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - function
+ * @d:input
+ * @name:input
+ * @age:input
+ * @owner:input
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+}
