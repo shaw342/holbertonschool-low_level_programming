c6d4cc3c45ac9e18fbc096dc93889ec4913137bd
shaw342 <shawanbarua70@gmail.com>
2023-03-21 03:45:07 -0700

    0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - function
+ * @name: input
+ * f:function
+ * @name:input
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	printf("%s", name);
+	f(name);
+
+}
