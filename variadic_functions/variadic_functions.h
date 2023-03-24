3ab0605f9acab28516356d7e6ca57d1001800b2d
shaw342 <shawanbarua70@gmail.com>
2023-03-24 14:35:07 -0700

    1,2,3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,4 +1,6 @@
 #ifndef HEADER
 #define HEADER
 int sum_them_all(const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 #endif
