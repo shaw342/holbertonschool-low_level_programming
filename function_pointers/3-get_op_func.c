b09d3ca2cb30ee7efcc580e1da52ce9f8535a3fa
shaw342 <shawanbarua70@gmail.com>
2023-03-24 04:09:07 -0700

    3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * get_op_func -  select the good fonction
+ * @s: An input char pointer operator passed as argument
+ * Return: the good poitner
+ **/
+int (*get_op_func(char *s))(int, int)
+{
+	  op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+
+    while (i < 5)
+    {
+	    if (*ops[i].op == *s && *(s + 1) == '\0')
+		return (ops[i].f);
+	    i++;
+
+    }
+    return (NULL);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * op_add - function add two integer
+ * @a: input
+ * @b: input
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function sub two integer
+ * @a: input
+ * @b: input
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function multiplie two integer
+ * @a: input
+ * @b: input
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function divide two integer
+ * @a: input
+ * @b: input
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - function modulo two integer
+ * @a: input
+ * @b: input
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	return (a % b);
+}
