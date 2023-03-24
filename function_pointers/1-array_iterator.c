d61a495bf8dce1f44b2c85ddad2d1db8c9a0d28b
shaw342 <shawanbarua70@gmail.com>
2023-03-24 05:49:14 -0700

    1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include "function_pointers.h"
+/**
+  * array_iterator - print name
+  * @array: string
+  * @size: containe size of string
+  * @action: pointer function
+  * Return: void
+  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
 	if (array && size && action)
 	{
-		while(i < size)
+		while (i < size)
 		{
 			action(array[i++]);
 		}
