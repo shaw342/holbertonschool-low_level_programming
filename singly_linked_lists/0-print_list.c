f66d71ef5f832bb594e89b141f21a76cef06783b
shaw342 <shawanbarua70@gmail.com>
2023-04-04 14:32:51 +0200

    add space

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,7 +7,7 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%u]",h->len);
+		printf("[%u] ",h->len);
 		if (h->str == NULL)
         	{
                 	printf("(nil)\n");
