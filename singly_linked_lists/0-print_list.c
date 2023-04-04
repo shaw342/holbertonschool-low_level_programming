5ebe25cd92210fff1588d5a6da51a0d6d613ab7a
shaw342 <shawanbarua70@gmail.com>
2023-04-04 14:42:30 +0200

    pritn liste

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,24 +1,28 @@
 #include "lists.h"
 #include <stdio.h>
+/**
+ * print_list - function print the single list
+ * @h: pointeur
+ * Return: the len
+ */
 size_t print_list(const list_t *h)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
 
-	while (h != NULL)
+	while (h)
 	{
-		printf("[%u] ",h->len);
+		printf("[%u] ", h->len);
 		if (h->str == NULL)
-        	{
-                	printf("(nil)\n");
-        	}else
-        	{
-                	printf("%s\n",h->str);
-        	}
+		{
+			printf("(nil)\n");
+		}
+		else
+		{
+			printf("%s\n", h->str);
+		}
 		h = h->next;
 		i++;
-
-
 	}
 		
 	return (i);
