d8248b05d299098516d8c45c859426cefa6fa6c4
shaw342 <shawanbarua70@gmail.com>
2023-04-05 16:17:23 +0200

    function return the length

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,10 @@
 #include "lists.h"
+
 /**
-  * dlistint_len - function returns the length of the list
-  * @h: input
-  * Return : length
-  */
+ * dlistint_len - function returns the length of the list
+ * @h: input
+ * Return : length
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i = 0;
