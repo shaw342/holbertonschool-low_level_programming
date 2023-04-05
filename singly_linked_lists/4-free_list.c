d05ca3289df38e4d1d34554c4741478db2c66334
shaw342 <shawanbarua70@gmail.com>
2023-04-05 14:18:21 +0200

    free

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+  * free_list - function that frees a list_t list
+  * @head: input
+  * Return: head
+  */ 
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+	free(head);
+}
