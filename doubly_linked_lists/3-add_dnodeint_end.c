a1fb9b22b07e247817006a2501cf35680cd7be6f
shaw342 <shawanbarua70@gmail.com>
2023-04-06 13:27:34 +0200

    kernel code

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+ * add_dnodeint_end - function the add node in the end list
+ * @head: pointers
+ * @n: integers
+ * Return: head
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *tmp, *new_node;
