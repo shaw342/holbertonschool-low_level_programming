87e872db8ab7c3670e38ec54e8e31f528e36fd45
shaw342 <shawanbarua70@gmail.com>
2023-04-06 15:39:20 +0200

    kernel code

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,9 +1,15 @@
 #include "lists.h"
+/*
+ * get_dnodeint_at_index - function restuen the index of liste
+ * @head: pointers
+ * @index: unsigned int
+ * Return: head
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	size_t i;
 
-	for (i = 0; head;i++)
+	for (i = 0; head; i++)
 	{
 		if (i == index)
 		{
