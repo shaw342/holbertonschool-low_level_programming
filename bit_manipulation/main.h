6d33948e96ff420bfe750f2320d02720ac3379d6
shaw342 <shawanbarua70@gmail.com>
2023-04-07 17:02:07 +0200

    modify main.h

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -2,6 +2,8 @@
 #define __MAIN__
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
