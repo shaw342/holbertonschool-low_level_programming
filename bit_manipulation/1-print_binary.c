e21d93de498f5b46785fd8d3d5093a7a7ac65eee
shaw342 <shawanbarua70@gmail.com>
2023-04-07 17:05:31 +0200

    print binary

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 /**
  * print_binary - function  that returns the value of a bit at a given index
- * @n: unsigned long int 
+ * @n: unsigned long int
  * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	
 	if (n > 1)
 		print_binary(n >> 1);
 
-        (n & 1) ? putchar('1') : putchar('0');
+	(n & 1) ? putchar('1') : putchar('0');
 }
