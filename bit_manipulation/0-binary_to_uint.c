97c119ccb5ddee5a70bf42495e06ac16dc1d2c78
shaw342 <shawanbarua70@gmail.com>
2023-04-07 15:28:17 +0200

    binaire to int

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * binary_to_uint - function 
+ * @b: integers
+ * Return: result
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	unsigned int result = 0;
+
+	while (b[i])
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+
+			result = result << 1;
+			result += (b[i] - '0');
+
+		}else
+			return (0);
+		i++;
+
+	}
+	return (result);
+
+}
