c3fb762024ca1dff3f8fc6b14be6938ad227db70
shaw342 <shawanbarua70@gmail.com>
2023-04-10 13:22:56 +0200

    function that read text file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * read_textfile - function reads a text file and prints it to the POSIX standard output
+ * @filename: name of file
+ * @letters: size of texte
+ * Return: wr
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, re, wr;
+	char *buf;
+
+	buf = malloc(letters * sizeof(char));
+
+	if (!buf)
+	{
+		return (0);
+	}
+
+	fd = open(filename,O_RDONLY);
+	re = read(fd,buf,letters);
+	wr = write(STDOUT_FILENO,buf,re);
+
+	if (fd == -1 || re == -1 || wr != re)
+	{
+		free(buf);
+		return (0);	
+	}
+	close(fd);
+	
+	return (wr);
+	
+}
---
+++ file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef __MAIN__
+#define __MAIN__
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif
