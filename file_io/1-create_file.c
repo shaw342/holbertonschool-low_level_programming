43af5955e8bb1843c54016759e74a6e5e585555b
shaw342 <shawanbarua70@gmail.com>
2023-04-10 17:35:28 +0200

    create file function

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[i])
 		i++;
 
-	if (!filename)
+	if (!filename || text_content == NULL)
 		return (-1);
 
 	fd = open(filename,O_RDWR | O_CREAT | O_TRUNC,S_IRUSR | S_IWUSR);
