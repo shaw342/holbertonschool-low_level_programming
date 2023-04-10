e9e62084e1bad2997dcac5011ca64880f243a3e3
shaw342 <shawanbarua70@gmail.com>
2023-04-10 17:33:16 +0200

    create file function

---
+++ file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file
+ * @filename: file
+ * @text_content: texte
+ * Return: 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i = 0;
+
+
+	while (text_content[i])
+		i++;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename,O_RDWR | O_CREAT | O_TRUNC,S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	write(fd,text_content,i);
+	
+	close(fd);
+
+	return (1);
+
+}
