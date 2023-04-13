93170be3b109ca79402152e68bf5404d23a037cd
shaw342 <shawanbarua70@gmail.com>
2023-04-13 11:24:30 +0200

    creat hashtable

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,16 +1,20 @@
 #include "hash_tables.h"
+/**
+ * hash_table_create - function create hashteble
+ * @size: input
+ * Return: hash_table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
-	unsigned long int i;
 
-	if (hast_table)
+	if (!hash_table)
 	{
 		return (NULL);
 	}
-	hast_table->size = size;
+	hash_table->size = size;
 
-	hash_table->array = malloc(size * sizeof(hash_node_t));
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
 
 	if (!hash_table->array)
 	{
@@ -19,6 +23,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	return (hash_table);
-
 
 }
